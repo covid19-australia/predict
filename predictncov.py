4be0217a1a8dc37fbef79cba0ca53bbc4a9122b1
Chen Chen <chen.chen@adelaide.edu.au>
2020-04-01 00:19:34 +1100

    change input format to cases every day
    No need for input of dates now.

--- predictncov.py
+++ predictncov.py
@@ -8,13 +8,7 @@ Prediction of confirm cases of coronavirus using local linear models
 
 Example of function inputs as numpy array are
 
-dates
- 
-array([ 0.,  2.,  4., 38., 39., 40., 41., 42., 43., 44., 45., 46., 47.,
-       48., 49., 50., 51., 52., 53., 54., 55., 56., 57., 58., 59., 60.,
-       61., 62., 63., 64., 65., 66., 67., 68.])
-    
-cases
+cases=
 
 array([   1,    3,    4,    6,    9,   15,   22,   25,   28,   36,   39,
          47,   61,   65,   78,   92,  112,  134,  171,  210,  267,  307,
@@ -22,19 +16,22 @@ array([   1,    3,    4,    6,    9,   15,   22,   25,   28,   36,   39,
        2032], dtype=int64)   
 
 OR use lists as input 
+
+[1, 1, 4, 4, 5, 5, 7, 9, 9, 10, 12, 12, 13, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 17, 17, 17, 20, 20, 20, 25, 25, 29, 33, 40, 51, 59, 63, 72, 79, 92, 112, 126, 157, 198, 247, 299, 377, 454, 567, 709, 847, 1073, 1353, 1681, 2051, 2432, 2806, 3179, 3639, 3983, 4249]
+
 """
 
-import pandas as pd
+
 import numpy as np
 
-
+'''
 data = pd.read_json(path_or_buf=('https://gist.githubusercontent.com/wileam/'
                                  '49d4c83efb6b554eb9ce773289dcc5f9/raw/edaa80'
                                  '199265af0278525cd46b8c33eec47aa2b4/'
                                  'NSWDailyHistorys.json'), orient='records')
 
 #hyperparameter from cross validation
-bandwidth=3.35
+
 
 # dates
 dates = (data['date'] - data['date'][0]).astype('timedelta64[D]').to_numpy()
@@ -42,13 +39,25 @@ dates = (data['date'] - data['date'][0]).astype('timedelta64[D]').to_numpy()
 #use cumulative cases if you want to predict cumulative.
 #use daily confirmed cases if you want to predict daily confirmed.
 cases = data['totalConfirmedNumber'].to_numpy()
+'''
 
+
+cases = [1, 1, 4, 4, 5, 5, 7, 9, 9, 10, 12, 12, 13, 14, 15, 15, 15, 15, 15, 
+         15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 17, 17, 17, 20, 20, 20, 
+         25, 25, 29, 33, 40, 51, 59, 63, 72, 79, 92, 112, 126, 157, 198, 247, 
+         299, 377, 454, 567, 709, 847, 1073, 1353, 1681, 2051, 2432, 2806, 
+         3179, 3639, 3983, 4249]
+
+cases = [1, 0, 3, 0, 1, 0, 2, 2, 0, 1, 2, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 
+         0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 0, 5, 0, 4, 4, 7, 11, 8, 4, 9, 
+         7, 13, 20, 14, 31, 41, 49, 52, 78, 77, 113, 142, 138, 226, 280, 328
+         , 370, 381, 374, 373, 460, 344, 266]
 
 #predict two day forward. The case number if changing quickly.
 #Statistics shows that extrapolating anything more than 2 days is not reliable.
-predict_date = np.hstack((dates,np.arange(1,3,1)+max(dates)))
 
-def local_linear(y=cases,x=dates,N_days_predict=2,h=3.35,weight=1):
+
+def local_linear(y=cases,N_days_predict=2,h=3.4,weight=1):
     '''
     Local linear estimation is a way to regress a 
     random variable y given random variable x in a non-parametric way. 
@@ -71,10 +80,10 @@ def local_linear(y=cases,x=dates,N_days_predict=2,h=3.35,weight=1):
     As we are cross validating, the matrix xx and so on are square matrices.
     Do not change N_days_predict to anything >2 for now.
     '''  
-    
+    dates = np.arange(0,len(cases)).T.astype(float)
     predict_date=np.hstack((dates,np.arange(1,1+N_days_predict,1)+max(dates)))
     
-    x = np.asarray(x).reshape((-1,1))    
+    x = np.asarray(dates).reshape((-1,1))    
     y = np.asarray(y).reshape((-1,1)) 
     
     xx = np.absolute(x-predict_date.T)  
@@ -97,9 +106,11 @@ def local_linear(y=cases,x=dates,N_days_predict=2,h=3.35,weight=1):
     return y_predict.astype(int)
 
 
-prediction = local_linear(cases,dates)
+prediction = local_linear(cases)
 
 import matplotlib.pyplot as plt
+dates = np.arange(0,len(cases))
+predict_date=np.hstack((dates,np.arange(1,1+2,1)+max(dates)))
 plt.plot(dates, cases)
 plt.plot(predict_date,prediction,'o')
 plt.show()
